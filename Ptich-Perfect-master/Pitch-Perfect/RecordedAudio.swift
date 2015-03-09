68e6622bffb1f938c1bf252ff83af9799fe6e160
David Fry <david.fry.tv@gmail.com>
2015-03-09 09:31:23 -0700

    Committed by Udacity

---
+++ Ptich-Perfect-master/Pitch-Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch-Perfect
+//
+//  Created by David on 3/6/15.
+//  Copyright (c) 2015 David Fry. All rights reserved.
+//
+
+import Foundation
+
+class RecoredAudio: NSObject
+{
+    var filePathURL: NSURL!
+    var title: String!
+}
